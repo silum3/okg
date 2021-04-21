b555ee3df4ce314ced8a1f2dd5bc64001b7bd34e
abc <silum3@gmail.com>
2021-04-21 22:53:48 +0530

    ym

--- src/main/java/com/mss/mss/ServletInitializer.java
+++ src/main/java/com/mss/mss/ServletInitializer.java
@@ -2,7 +2,8 @@ package com.mss.mss;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+import org.springframework.stereotype.Controller;
+@Controller
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override

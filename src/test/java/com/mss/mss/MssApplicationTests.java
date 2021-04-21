9dba9691da9270336840404749da8574fede171c
abc <silum3@gmail.com>
2021-04-21 19:58:25 +0530

    first commit

---
+++ src/test/java/com/mss/mss/MssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mss.mss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

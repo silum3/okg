8e7e53be42d765c36817677487a49ad897aa4b36
silum3 <47265696+silum3@users.noreply.github.com>
2021-05-03 00:12:00 +0530

    e1
    e1

--- src/main/java/com/mss/mss/MssApplication.java
+++ src/main/java/com/mss/mss/MssApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MssApplication {
 
 	public static void main(String[] args) {
+		System.out.println(12);
 		SpringApplication.run(MssApplication.class, args);
 	}
 
